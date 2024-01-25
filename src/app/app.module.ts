de407eb99698f971b4030584aa30126b9d422ab3
thanhminhspk05 <69253365+thanhminhspk05@users.noreply.github.com>
2024-01-25 15:43:20 +0700

    lesson 52

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,35 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { DetailProductComponent } from './detail-product/detail-product.component';
-import { OrderComponent } from './order/order.component';
-import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { DetailProductComponent } from "./detail-product/detail-product.component";
+import { FooterComponent } from "./footer/footer.component";
+import { HeaderComponent } from "./header/header.component";
+import { HomeComponent } from "./home/home.component";
+import { LoginComponent } from "./login/login.component";
+import { OrderConfirmComponent } from "./order-confirm/order-confirm.component";
+import { OrderComponent } from "./order/order.component";
+import { RegisterComponent } from "./register/register.component";
 
 @NgModule({
-  declarations: [    
-    HomeComponent, 
+  declarations: [
+    HomeComponent,
     HeaderComponent,
-    FooterComponent, 
-    DetailProductComponent, OrderComponent, OrderConfirmComponent, LoginComponent, RegisterComponent
+    FooterComponent,
+    DetailProductComponent,
+    OrderComponent,
+    OrderConfirmComponent,
+    LoginComponent,
+    RegisterComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [
-    //HomeComponent,
-    //DetailProductComponent,
+    // HomeComponent,
+    // DetailProductComponent,
     // OrderComponent,
-    //OrderConfirmComponent,
+    // OrderConfirmComponent,
     // LoginComponent,
-    RegisterComponent
-  ]
+    RegisterComponent,
+  ],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,10 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { RegisterDTO } from "../dtos/user/register.dto";
+import { UserService } from "./../services/user.service";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.scss"],
 })
 export class RegisterComponent {
+  @ViewChild("registerForm") registerForm!: NgForm;
 
+  phone: string;
+  password: string;
+  retypePassword: string;
+  fullName: string;
+  address: string;
+  isAccepted: boolean;
+  dateOfBirth: Date;
+
+  constructor(private userService: UserService) {
+    this.phone = "";
+    this.password = "";
+    this.retypePassword = "";
+    this.fullName = "";
+    this.address = "";
+    this.isAccepted = false;
+    this.dateOfBirth = new Date();
+    this.dateOfBirth.setFullYear(this.dateOfBirth.getFullYear() - 18);
+  }
+
+  register() {
+    const registerData: RegisterDTO = {
+      fullname: this.fullName,
+      phone_number: this.phone,
+      address: this.address,
+      password: this.password,
+      retype_password: this.retypePassword,
+      date_of_birth: this.dateOfBirth,
+      facebook_account_id: 0,
+      google_account_id: 0,
+      role_id: 1,
+    };
+    this.userService.register(registerData);
+  }
+
+  checkPasswordMatch() {
+    if (this.password !== this.retypePassword) {
+      this.registerForm.form.controls["retypePassword"].setErrors({
+        passwordMismatch: true,
+      });
+    } else {
+      this.registerForm.form.controls["retypePassword"].setErrors(null);
+    }
+  }
+
+  checkAge() {
+    if (this.dateOfBirth) {
+      const today = new Date();
+      const birthDate = new Date(this.dateOfBirth);
+      let age = today.getFullYear() - birthDate.getFullYear();
+      const monthDiff = today.getMonth() - birthDate.getMonth();
+      if (
+        monthDiff < 0 ||
+        (monthDiff === 0 && today.getDate() < birthDate.getDate())
+      ) {
+        age--;
+      }
+
+      if (age < 18) {
+        this.registerForm.form.controls["dateOfBirth"].setErrors({
+          invalidAge: true,
+        });
+      } else {
+        this.registerForm.form.controls["dateOfBirth"].setErrors(null);
+      }
+    }
+  }
 }
